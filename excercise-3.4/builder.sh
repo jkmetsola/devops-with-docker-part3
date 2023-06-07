e2e772e003be8959f577acb257f36d7d54e95eda
Jaakko Metsola <jaakko.metsola@nokia.com>
2023-06-07 13:39:06 +0300

    feat: excercise-3.4

---
+++ excercise-3.4/builder.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# command line argument $1 = repo/imagename. (e.g. jkmetsola/excercise-3.3)
+docker logout
+cat DOCKER_SECRET.txt | docker login --username jkmetsola --password-stdin  # DOCKER_SECRET.txt contains password/API key
+docker build --tag $1 .
+docker push $1
